30ef8c4e24d73c28737e916cbadd0851ff44c730
xanderazuaje <xander@xanderazuaje.com>
2023-10-17 23:08:30 +0200

    feature: add economic module's input

---
+++ structs/http-requests/economic-input.go
@@ -0,0 +1,16 @@
+package httpReq
+
+import (
+    "github.com/Sanzar-Group/std-model/structs"
+)
+
+type production struct {
+    Expected int `json:"expected"`
+    Previous int `json:"previous"`
+}
+
+type EconomicInput struct {
+    NPK structs.NPK `json:"npk"`
+    Ha int `json:"ha"`
+    Production production `json:"production"`
+}
