f068760dcefe5db1e75d120bf097465623156cfc
xanderazuaje <xander@xanderazuaje.com>
2023-10-20 00:13:45 +0200

    fix: typo

--- structs/aspicious-planting-date/optimal-date.go
+++ structs/aspicious-planting-date/optimal-date.go
@@ -2,5 +2,5 @@ package aspiciousPlanting
 
 type OptimalDate struct {
     Temperature []ReferencedDate `json:"temperature"`
-    Humidity []referencedDate `json:"humidity"`
+    Humidity []ReferencedDate `json:"humidity"`
 }
